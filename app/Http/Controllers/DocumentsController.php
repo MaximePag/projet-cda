45fda4bb625b92b6214fc51311d62020eb1e5073
Jérôme CLAUET <clauet.jerome@hotmail.fr>
2021-08-27 10:02:32 +0200

    crudDocuments

---
+++ app/Http/Controllers/DocumentsController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Documents;
+use Illuminate\Http\Request;
+
+class DocumentsController extends Controller
+{
+
+    public function index()
+    {
+        $documents = Documents::all();
+        return response()->json($documents);
+    }
+
+    public function create(Request $request)
+    {        
+        $documents = new Documents();
+
+        $documents->title = $request->title;
+        $documents->path = $request->path;
+        $documents->uploadDate = $request->uploadDate;
+
+        $documents->save();
+
+        return response()->json("Votre/Vos documents ont bien été enregistrer.");
+    }
+
+    public function store(Request $request)
+    {
+        
+    }
+
+    public function show($id)
+    {
+        $documents = Documents::find($id);
+        return response()->json($documents);
+    }
+
+    public function edit(Documents $documents)
+    {
+        
+    }
+
+    public function update(Request $request, $id)
+    {
+        $documents = Documents::find($id);
+
+        $documents->title = $request->title;
+        $documents->path = $request->path;
+        $documents->uploadDate = $request->uploadDate;
+
+        $documents->save();
+
+        return response()->json($documents);
+    }
+
+    public function destroy($id)
+    {
+        $documents = Documents::find($id);
+        $documents->delete();
+
+        return response()->json('Votre/Vos documents ont bien été supprimer.');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,15 @@
 <?php
 
-/** @var \Laravel\Lumen\Routing\Router $router */
-
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It is a breeze. Simply tell Lumen the URIs it should respond to
-| and give it the Closure to call when that URI is requested.
-|
-*/
 $router->get('/', function () use ($router) {
     $results = app('db')->select("SELECT * FROM users");
 
      return $results;
 });
+
+// route pour la table Documents
+
+$router->get('Documents', 'DocumentsController@index' );
+$router->get('/Documents/{id}', 'DocumentsController@show');
+$router->post('/Documents', 'DocumentsController@create');
+$router->put('/Documents/{id}', 'DocumentsController@update');
+$router->delete('/Documents/{id}', 'DocumentsController@delete');
